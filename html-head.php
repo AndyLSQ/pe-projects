8315fcf02b7b92a143b47fa4459e65c5120b0fc6
Andy <andrew.eliscu@gmail.com>
2022-05-16 18:01:46 -0500

    Personal page- new version

--- html-head.php
+++ html-head.php
@@ -10,6 +10,9 @@
 	<meta property="og:image" content="https://peprojects.dev/alpha-4/andy/base/images/default-meta.png">
 
 	<!-- LINK fonts as needed -->
+	<!-- <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@900&family=Source+Sans+Pro&display=swap" rel="stylesheet"> -->
+
+
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
 
 	<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=DM+Serif+Text&family=Kreon&display=swap" rel="stylesheet">
--- index.php
+++ index.php
@@ -8,64 +8,73 @@
 		<h1>Hi, <br>I’m Andy.</h1>
 
 		<!-- use &nbsp; to pair items for line breaks -->
-		<h3>A web developer with an appreciation for clean, responsive design.</h3>
+		<h4>I design websites with a focus on clean, responsive design.</h4>
 
-		<a href="#contact">Contact Me</a>
+		<a href="#about">Learn more</a>
 
 
 	</div>
 </header>
 
-
-<section class="projects">
-	<div class="inner-column">
-
-		<h2>My projects</h2>
-		<div class="cards">
-			<div class="card">
-				<h3>Project 1</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<a href="#">See project</a>
-			</div>
-
-			<div class="card">
-				<h3>Project 2</h3>
-				<p>Lorem ipsum dolor sit amet, eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<a href="#">See project</a>
-			</div>
-
-			<div class="card">
-				<h3>Project 3</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore et dolore magna aliqua. </p>
-				<a href="#">See project</a>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class='about'>
+<section class='about' id=about>
 	<div class="inner-column">
 
 		<h2>About me</h2>
 			
 		<bio>
 
-			<picture>
-				<img src="https://peprojects.dev/images/portrait.jpg">
-			</picture>
+			<!-- <picture>
+				<img src="images/headshot.png">
+			</picture> -->
 
 			<bio-text>
-				<p>General background info, e.g. location. Sentence about my work experience. Sentence about my work experience. Sentence about my web development experience.</p>
+				<h3>Who am I?</h3>
+				<p>I’m a web development student based in Austin, Texas, and currently under the mentorship of <a href="https://perpetual.education/" target="pe">Perpetual Education</a>, a program covering the entire design and web development process from idea to completion.</p>
+				<p>Follow my learning journey on <a href="https://lisq.substack.com/" target="blog">Substack</a>.</p>
 
-				<p> Sentence to elaborate on my development experience. Sentence to elaborate on my development experience. Check out my <a href="https://lisq.substack.com/">Substack</a> blog to follow along on my learning journey.</p>
+				<h3>Where have I been?</h3>
+				<p>I have 10 years of prior experience in Financial technology, serving large corporate clients in Finance Process Architecture and Software Implementation.</p>
+				<p> Check out my full <a href="#resume" target="resume">resume</a>.</p>
 
-				<p>Sentence about my goals, sentence about my goals. Another sentence about my goals, maybe longer than the last one. <a href="https://peprojects.dev/alpha-4/andy/goals/">Click here</a> to learn more about my goals.</p>
+
+				<h3>Why am I here?</h3>
+				<p> I’m here to pursue a new career path that aligns with my lifelong interest in design and technology.</p>
+
+				<p> Take an in-depth look at my <a href="https://peprojects.dev/alpha-4/andy/goals/" target="goals">goals</a>.</p>
 				
 
 			</bio-text>
 		</bio>
 	</div>
 </section>
+
+<section class="projects" id=projects>
+	<div class="inner-column">
+
+		<h2>My projects</h2>
+		<div class="cards">
+			<div class="card">
+				<h3>Come To You</h3>
+				<p>Case Study: A mobile mechanic needs to build brand awareness and quickly explain their business to prospective customers.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/small-business/">See project</a>
+			</div>
+
+			<div class="card">
+				<h3>Responsive Layout</h3>
+				<p>Challenge: Create a page layout that adapts arrangement of its content based on viewport width.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/super-layout-challenge/">See project</a>
+			</div>
+
+			<div class="card">
+				<h3>Brine Lacrosse</h3>
+				<p>Challenge: Recreate the layout of a commercial website, with particular focus on the spacing of elements.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/05.06-brine-spacing-challenge/">See project</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
 
 
 <section class='contact' id='contact'>
@@ -79,13 +88,13 @@
 
 			<social>
 
-				<a href="mailto:andrew.eliscu@gmail.com">
+				<a href="mailto:andrew.eliscu@gmail.com" target="email">
 					<icon>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/></svg>
 					</icon>
 				</a>
 
-				<a href="https://github.com/AndyLSQ">
+				<a href="https://github.com/AndyLSQ" target="github">
 					<icon>
 						<svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
 					</icon>
@@ -97,7 +106,7 @@
 					</icon>
 				</a>
 
-				<a href="https://lisq.substack.com/">
+				<a href="https://lisq.substack.com/" target="blog">
 					<icon>
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 24l-5-4.39-5 4.39v-20h10v20zm-12-22h8v-2h-10v20l2-1.756v-16.244z"/></svg>
 					</icon>

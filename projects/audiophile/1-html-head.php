8ac0e3d0e11805a2867dc3649d9bc4adeef6789b
Andy <andrew.eliscu@gmail.com>
2022-06-05 12:00:03 -0500

    Audiophile head and metadata update

--- projects/audiophile/1-html-head.php
+++ projects/audiophile/1-html-head.php
@@ -3,12 +3,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<title>BOILERPLATE</title>
+	<title>Audophile Store</title>
 	
-	<meta name="description" content="[[INSERT DESCRIPTION]]">
+	<meta name="description" content="High end headphones and speakers">
 
 	<!-- Note: Standard metadata image size ~(1200 x 630) -->
-	<meta property="og:image" content="https://peprojects.dev/alpha-4/andy/base/images/default-meta.png">
+	<meta property="og:image" content="https://is3-ssl.mzstatic.com/image/thumb/Purple122/v4/48/03/34/480334c0-4cea-d293-89f2-3521e84f876a/source/256x256bb.jpg">
 
 	<!-- LINK fonts as needed -->
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">

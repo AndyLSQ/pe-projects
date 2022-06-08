03b37d601fea7a1f1fd91ed2e3ee223bb166e2ff
Andy <andrew.eliscu@gmail.com>
2022-06-08 17:08:44 -0400

    Index to include notes page

--- projects/e4p/index.php
+++ projects/e4p/index.php
@@ -5,11 +5,13 @@
 <h1>Exercises For Programmers</h1>
 <h2>Exercise Index</h2>
 <ul>
+	<li><a href="lesson-notes.md" target="notes">Lesson Notes</a></li>
 	<li><a href="01-hello.php" target="exercise">Hello World</a></li>
 	<li><a href="03-printing-quotes.php" target="exercise">Printing Quotes</a></li>
 	<li><a href="05-simple-math.php" target="exercise">Simple Math</a></li>
 	<li><a href="06-retirement-calculator.php" target="exercise">Retirement Calculator</a></li>
 	<li><a href="07-room-area.php" target="exercise">Room Area</a></li>
 	<li><a href="08-pizza-party.php" target="exercise">Pizza Party</a></li>
+
 
 </ul>

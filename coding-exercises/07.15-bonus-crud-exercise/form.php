660c1818df4b360a5b046aa4846e5fbf8ecbb2d6
Andy <andrew.eliscu@gmail.com>
2022-07-15 17:25:27 -0500

    CRUD bonus exercise

---
+++ coding-exercises/07.15-bonus-crud-exercise/form.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<?php
+	$id = "";
+
+
+	$submittedPOST = isset($_POST['submitted']);
+
+	if($submittedPOST) {
+		
+		$title = $_POST['title'];
+		$artist = $_POST['artist'];
+		$album = $_POST['album'];
+		
+
+
+		echo "Vehicle " . $id . " submitted";
+		echo "Price is " . $price;
+
+	}
+
+	$id = uniqid("a");
+
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vehicle Input Form</title>
+
+	<link rel="stylesheet" href="style/style.css">
+</head>
+
+
+<body>
+<inner-column>
+
+
+<h1>Vehicle Input Form</h1>
+
+<form method="POST">
+	<field>
+		<label>Vehicle ID</label>
+		<input type="text" name="id" value="<?=$id?>" disabled>
+	</field>
+
+	<field>
+		<label>Bodystyle</label>
+		<select name="bodystyle" required>
+			<option disabled selected value> -- Select an option --</option>
+			<option value="suv">SUV</option>
+			<option value="sedan">Sedan</option>
+			<option value="performance">Performance</option>
+		</select>
+	</field>
+
+	<field>
+		<label>Size</label>
+		<select name="size" required>
+			<option disabled selected value> -- Select an option --</option>
+			<option value="subcompact">Subcompact</option>
+			<option value="compact">Compact</option>
+			<option value="midsize">Midsize</option>
+			<option value="fullsize">Fullsize</option>
+		</select>
+	</field>
+
+	<field>
+		<label>Seating Capacity</label>
+		<input type="number" name="capacity" value="<?=$capacity?>"  required min=2 max=9 placeholder="2">
+	</field>
+
+	<field>
+		<label>Year</label>
+		<input type="number" name="year" value="<?=$year?>"  required min=2021 max=2023 placeholder="2021">
+	</field>
+
+	<field>
+		<label>Make</label>
+		<input type="text" name="make" value="<?=$make?>"  required>
+	</field>
+
+	<field>
+		<label>Model</label>
+		<input type="text" name="model" value="<?=$model?>"  required>
+	</field>
+
+	<field>
+		<label>Tagline</label>
+		<textarea type="textarea" rows=4 cols=30 name="tagline" value="<?=$tagline?>"  required> </textarea>
+	</field>
+
+	<field>
+		<label>Model Description</label>
+		<textarea type="textarea" rows=4 cols=30 name="description" value="<?=$description?>"  required> </textarea>
+	</field>
+
+	<field>
+		<label>Trim</label>
+		<select name="trim" required>
+			<option disabled selected value> -- Select an option --</option>
+			<option value="base">Base</option>
+			<option value="premium">Premium</option>
+			<option value="limited">Limited</option>
+			<option value="touring">Touring</option>
+		</select>
+	</field>
+
+	<field>
+		<label>Price</label>
+		<input type="number" name="price" value="<?=$price?>"  required>
+	</field>
+
+	<button type="submit" name="submitted">
+		Submit Vehicle
+	</button>
+
+
+</form>
+
+</inner-column>
+</body>
+
+</html>
--- coding-exercises/07.15-bonus-crud-exercise/index.php
+++ coding-exercises/07.15-bonus-crud-exercise/index.php
@@ -28,51 +28,113 @@
 		return $parts[1];
 	}
 
+	function showDeleteId() {
+		$delete = showUrl();
+		$deleteId = explode('=', showUrl())[1];
+		// print_r($deleteId);
+		return $deleteId;
+
+	}
+
 
 
 	// INITIAL VALUES
 
-	$nameInput = ""; // reset the input
-
+	$titleInput = ""; // reset the input
+	$artistInput = "";
+	$albumInput = "";
 
 
 	// ACTIONS
 
 	function initializeDatabase() {
 		// create a data structure you want to use as your base
+		$database = [
+			"tracklist" => []
+		];
 		// encode that to json
+		$json = json_encode($database);
+
 		// save it to the file
+		file_put_contents("tracklist.json", $json);
+
+
 		// return the default datastructure (PHP-style)
+		return $database;
+
 	}
+
+	// print_r(initializeDatabase());
 
 	function getDatabase() {
 		// check if database file exists and has content
+		if ( file_get_contents("tracklist.json") ) {
+			// echo "Database initialized";
+		
 		// if it does
 			// get the content
+			$json = file_get_contents("tracklist.json");
 			// decode it
+			$database = json_decode($json, true);
 			// return the database
+			return $database;
 		// if it doesn't 
+		}
+		else{
+			// echo "Database not intialized";
 			// initialize the database
+			$database = initializeDatabase();
 			// and return it's data (PHP-style)
+			return $database;
+		}
 	}
+	// getDatabase(); //this will see if theres a database (and create one if not, then return the database)
+
 
 	function createRecord($input) {
 		// create a unique id
+		$id = uniqid("a");
 		// get the data so you can add to it
+		$database = getDatabase();
+
 		// put the input into the database (based on the structure you chose)
+	
+		$database['tracklist'][$id] = [
+			"title" => $input['title'],
+			"artist" => $input['artist'],
+			"album" => $input['album']
+		];
+
 		// save the updated database
+		saveData($database);
 	}
 
 	function saveData($data) {
 		// convert it to json
+		$json = json_encode($data);
 		// save it
+		file_put_contents("tracklist.json" ,$json);
 	}
+
+	// createRecord([
+	// 		"title" => "this is a title",
+	// 		"artist" => "nsync",
+	// 		"album" => "the album"
+	// ]);
 
 	function deleteRecordById($id) {
 		// get database
+		$database = getDatabase();
+
 		// unset (delete) the property (id) in that database
+		unset ($database['tracklist'][$id]);
+
 		// save the database
+		saveData($database);
+
 	}
+
+	deleteRecordById("a62d1dfd6d8f08");
 
 	// **bonus**
 	// clearDatabase()
@@ -91,10 +153,16 @@
 
 	// RENDER THINGS
 
-	function renderRecord($id, $name) {
+	function renderRecord($id, $record) {
+		$title = $record['title'];
+		$artist = $record['artist'];
+		$album = $record['album'];
+		
 		echo "
 			<li>
-				<h2>$name</h2>
+				<h2>Title: $title</h2>
+				<p>Artist: $artist</p>
+				<p>Album: $album</p>
 
 				<a href='?delete=$id'>delete</a>
 			</li>
@@ -103,9 +171,12 @@
 
 	function renderRecords($records) {
 		// render the records
-		$names = array_reverse( $records["items"] );
-		foreach ($names as $id => $name) {
-			renderRecord($id, $name);
+		$titles = array_reverse( $records["tracklist"] );
+		foreach ($titles as $id => $record) {
+			// print_r("The ID is: $id");
+			// print_r("The song info is: ");
+			// print_r($record);
+			renderRecord($id, $record);
 		}
 	}
 
@@ -125,8 +196,9 @@
 	// if the user added a name
 	if ( isset($_POST["submitted"]) ) {
 		// if they added text
-		if ( $_POST["name"] && strlen($_POST["name"]) > 0 ) {
+		if ( $_POST["title"] && strlen($_POST["title"]) > 0 ) {
 			// create the record
+			createRecord($_POST);
 		} else {
 			// if not, signify there's an error
 		}
@@ -135,6 +207,10 @@
 	// if the user deleted something
 	if ( isset( $_GET["delete"] ) ) {
 		// delete the record
+		$id = showDeleteId();
+		//echo "id to be deleted is: $id";
+		deleteRecordById($id);
+
 	}
 	
 ?>
@@ -147,17 +223,24 @@
 	<h1>Single-file CRUD exercise</h1>
 </a>
 
-<p><?=showUrl();?></p>
-
-
-<h2>Adding names to a list</h2>
+<h2>Adding songs to a list</h2>
 
 
 <form method='POST'>
 
 	<field>
-		<label>Name <?=showError($missingText)?></label>
-		<input name='name' value='<?=$nameInput?>'>
+		<label>Title <?=showError($missingText)?></label>
+		<input name='title' value='<?=$titleInput?>'>
+	</field>
+
+	<field>
+		<label>Artist <?=showError($missingText)?></label>
+		<input name='artist' value='<?=$artistInput?>'>
+	</field>
+
+	<field>
+		<label>Album <?=showError($missingText)?></label>
+		<input name='album' value='<?=$albumInput?>'>
 	</field>
 
 	<footer>

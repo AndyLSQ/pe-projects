0cfc38d895537de45e262af3547ddc4c6885e7cf
Andy <andrew.eliscu@gmail.com>
2022-07-18 13:47:58 -0500

    Medley Day 1 Layout

---
+++ coding-exercises/07.18-medley-week/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Medley Week</title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+	<header>
+		
+	</header>
+
+
+	<main>
+	
+	<?php include("templates/pages/layout1.php") ?>	
+
+
+	</main>
+
+
+</body>
+
+
+</html>
---
+++ coding-exercises/07.18-medley-week/templates/pages/layout1.php
@@ -0,0 +1,99 @@
+<div class="layout">
+<inner-column class="layout1">
+
+<div class="text">
+
+	<div class="page-header">
+		<h2>Heading level 2</h2>
+		<p>This is some body text. This is
+		some body text. This is some body
+		text. This is some body text.</p>
+	</div>
+
+
+	<ul class="articles">
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+		<li class="article">
+			<h3>Heading level 3</h3>
+			<p>This is some body text. This is some
+			body text. This is some body text. This
+			is some body text.</p>
+		</li>
+
+	</ul>
+
+</div>
+
+
+
+<div class="gallery">
+
+	<ul class="images">
+
+		<li class="image">
+			<picture>
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
+			</picture>
+		</li>
+
+		<li class="image">
+			<picture>
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
+			</picture>
+		</li>
+
+		<li class="image">
+			<picture>
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
+			</picture>
+		</li>
+
+		<li class="image">
+			<picture>
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
+			</picture>
+		</li>
+
+	</ul>
+
+</div>
+
+
+
+</inner-column>
+</div>

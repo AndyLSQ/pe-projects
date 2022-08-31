b0e9a954dabfbe35233b18855012085b2def0771
Andy <andrew.eliscu@gmail.com>
2022-08-31 18:00:45 -0500

    Move new personal page to root folder

---
+++ archive/personal-page-v2.1/index.php
@@ -0,0 +1,92 @@
+
+<?php include('site-header.php'); ?>
+
+
+<!-- main class="page-content" -->
+<header class='page-header'>
+	<div class="inner-column">
+		<h1>Hi, <br>I’m Andy.</h1>
+
+		<!-- use &nbsp; to pair items for line breaks -->
+		<h4>I make websites with a focus on clean, responsive design.</h4>
+
+		<a href="#about">Learn more</a>
+
+
+	</div>
+</header>
+
+<section class='about' id=about>
+	<div class="inner-column">
+
+		<h2>About me</h2>
+			
+		<bio>
+
+			<!-- <picture>
+				<img src="images/headshot.png">
+			</picture> -->
+
+			<bio-text>
+				<h3>Who am I?</h3>
+
+				<bio-answer>
+					<p>I’m a web development student based in Austin, Texas, and currently under the mentorship of <a href="https://perpetual.education/" target="pe">Perpetual Education</a>, a program covering the entire design and web development process from idea to completion.</p>
+					<p>Follow my learning journey on <a href="https://lisq.substack.com/" target="blog">Substack</a>.</p>
+				</bio-answer>
+
+
+				<h3>Where have I been?</h3>
+				<bio-answer>
+					<p>I have 10 years of prior experience in Financial technology, serving large corporate clients in Finance Process Architecture and Software Implementation.</p>
+					<p> Check out my full <a href="#resume" target="resume">resume</a>.</p>
+				</bio-answer>
+
+
+				<h3>Why am I here?</h3>
+
+				<bio-answer>
+					<p> I’m here to pursue a new career path that aligns with my lifelong interest in design and technology.</p>
+
+					<p> Take an in-depth look at <a href="https://peprojects.dev/alpha-4/andy/goals/" target="goals">my goals</a>.</p>
+				</bio-answer>
+
+			</bio-text>
+		</bio>
+	</div>
+</section>
+
+<section class="projects" id=projects>
+	<div class="inner-column">
+
+		<h2>My projects</h2>
+		<div class="cards">
+			<div class="card">
+				<h3>Come to You Case Study</h3>
+				<p>A mobile mechanic needs to build brand awareness and quickly explain their business to prospective customers.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/small-business/" target="cty">See project</a>
+			</div>
+
+			<div class="card">
+				<h3>Responsive Layout Challenge</h3>
+				<p>Create a page layout that adapts arrangement of its content based on viewport width.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/super-layout-challenge/" target="super-layout">See project</a>
+			</div>
+
+			<div class="card">
+				<h3>Brine Lacrosse Challenge</h3>
+				<p>Recreate the layout of a commercial website, with particular focus on the spacing of elements.</p>
+				<a href="https://peprojects.dev/alpha-4/andy/projects/05.06-brine-spacing-challenge/" target="brine">See project</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+<!-- /main class="page-content" -->
+
+<?php include('site-footer.php'); ?>
--- index.php
+++ index.php
@@ -1,92 +1,67 @@
+<!DOCTYPE html>
+<html lang='en'>
 
-<?php include('site-header.php'); ?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<meta name="description" content="Andy is a front-end web developer with a passion for creating clean, responsive websites.">
 
-<!-- main class="page-content" -->
-<header class='page-header'>
-	<div class="inner-column">
-		<h1>Hi, <br>I’m Andy.</h1>
+	<!-- Note: Standard metadata image size ~(1200 x 630) -->
+	<meta property="og:image" content="https://peprojects.dev/alpha-4/andy/projects/personal-page-pro/images/metadata-image.png">
 
-		<!-- use &nbsp; to pair items for line breaks -->
-		<h4>I make websites with a focus on clean, responsive design.</h4>
+	<title>Andy LSQ | Web Developer</title>
 
-		<a href="#about">Learn more</a>
+	<link rel="stylesheet" href="styles/site.css">
 
-
-	</div>
-</header>
-
-<section class='about' id=about>
-	<div class="inner-column">
-
-		<h2>About me</h2>
-			
-		<bio>
-
-			<!-- <picture>
-				<img src="images/headshot.png">
-			</picture> -->
-
-			<bio-text>
-				<h3>Who am I?</h3>
-
-				<bio-answer>
-					<p>I’m a web development student based in Austin, Texas, and currently under the mentorship of <a href="https://perpetual.education/" target="pe">Perpetual Education</a>, a program covering the entire design and web development process from idea to completion.</p>
-					<p>Follow my learning journey on <a href="https://lisq.substack.com/" target="blog">Substack</a>.</p>
-				</bio-answer>
-
-
-				<h3>Where have I been?</h3>
-				<bio-answer>
-					<p>I have 10 years of prior experience in Financial technology, serving large corporate clients in Finance Process Architecture and Software Implementation.</p>
-					<p> Check out my full <a href="#resume" target="resume">resume</a>.</p>
-				</bio-answer>
-
-
-				<h3>Why am I here?</h3>
-
-				<bio-answer>
-					<p> I’m here to pursue a new career path that aligns with my lifelong interest in design and technology.</p>
-
-					<p> Take an in-depth look at <a href="https://peprojects.dev/alpha-4/andy/goals/" target="goals">my goals</a>.</p>
-				</bio-answer>
-
-			</bio-text>
-		</bio>
-	</div>
-</section>
-
-<section class="projects" id=projects>
-	<div class="inner-column">
-
-		<h2>My projects</h2>
-		<div class="cards">
-			<div class="card">
-				<h3>Come to You Case Study</h3>
-				<p>A mobile mechanic needs to build brand awareness and quickly explain their business to prospective customers.</p>
-				<a href="https://peprojects.dev/alpha-4/andy/projects/small-business/" target="cty">See project</a>
-			</div>
-
-			<div class="card">
-				<h3>Responsive Layout Challenge</h3>
-				<p>Create a page layout that adapts arrangement of its content based on viewport width.</p>
-				<a href="https://peprojects.dev/alpha-4/andy/projects/super-layout-challenge/" target="super-layout">See project</a>
-			</div>
-
-			<div class="card">
-				<h3>Brine Lacrosse Challenge</h3>
-				<p>Recreate the layout of a commercial website, with particular focus on the spacing of elements.</p>
-				<a href="https://peprojects.dev/alpha-4/andy/projects/05.06-brine-spacing-challenge/" target="brine">See project</a>
-			</div>
-		</div>
-	</div>
-</section>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Display:ital@0;1&family=DM+Serif+Text:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
 
 
+</head>
+
+
+<?php 
+
+	include('functions.php'); 
+
+//Determine the active page (function)
+	$pageId = currentPage();
+	// echo $pageId;
+
+//Get data for the page (function)
+	$pageData = pageData();
+	// print_r($pageData)
+
+?>
+
+
+<body class="<?=$pageId?>">
+	 
+	<?php
+
+	if ($pageId == "project") {
+		include('templates/pages/project.php');
+	}
+	elseif ($pageId == "exercise") {
+		include('templates/pages/exercise.php');
+	}
+	elseif ($pageId == "layout") {
+		include('templates/pages/layout.php');
+	}
+	elseif ($pageId =="resume") {
+		include 'templates/pages/resume.php';
+	}
+	else {
+		include('templates/pages/default-page.php');
+	}
+
+	include('templates/modules/footer.php');
+
+	?>
 
 
 
-<!-- /main class="page-content" -->
+</body>
 
-<?php include('site-footer.php'); ?>
+</html>

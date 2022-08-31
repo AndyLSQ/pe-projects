afbdaa3bc1aa5d44f4e4b9b187d35aad0293b793
Andy <andrew.eliscu@gmail.com>
2022-08-31 12:10:25 -0500

    Create JS practice folder

--- projects/e4p-js/e4p.js
+++ projects/e4p-js/e4p.js
@@ -291,5 +291,3 @@ var selfCheckoutButton = document.createElement('button');
 selfCheckoutButton.addEventListener('click', selfCheckout);
 selfCheckoutButton.textContent = "Self Checkout";
 document.querySelector('body').appendChild(selfCheckoutButton);
-
-sadfjsfdlakjf(

1bc1260e818b810f5cc1358ab8f2175c1cf8d718
Andy <andrew.eliscu@gmail.com>
2022-08-31 15:51:48 -0500

    PPP- load metadata image

--- projects/personal-page-pro/index.php
+++ projects/personal-page-pro/index.php
@@ -5,11 +5,16 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>--PERSONAL PAGE PRO--</title>
+	<meta name="description" content="Andy is a front-end web developer with a passion for creating clean, responsive websites.">
+	<meta property="og:image" content="images/svg/metadata-img.svg">
+
+	<title>Andy LSQ | Web Developer</title>
 
 	<link rel="stylesheet" href="styles/site.css">
 
 	<link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Display:ital@0;1&family=DM+Serif+Text:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+
 
 </head>
 

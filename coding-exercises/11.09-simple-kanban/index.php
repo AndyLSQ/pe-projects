0cf897b36ebb8a09a38a387a1245f8ca1180d432
Andy <andrew.eliscu@gmail.com>
2022-11-09 13:42:05 -0600

    Kanban project - html

---
+++ coding-exercises/11.09-simple-kanban/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Kanban Board</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+
+<body>
+	<div class="kanban">
+		<!-- <div class="kanban__column">
+			<div class="kanban__column-title">Not Started</div>
+			<div class="kanban__items">
+				<div contenteditable class="kanban__item-input">Wash the dishes</div>
+				<div class="kanban__dropzone"></div>
+			</div>
+			<button class="kanban__add-item" type="button">+ Add</button>
+		</div> -->
+	</div>
+
+
+	<script src="js/main.js" type="module"></script>
+</body>
+</html>

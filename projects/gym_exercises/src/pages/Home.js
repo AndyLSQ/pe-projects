510c36fb4e44452ec959a920ab6d591d5308bec5
Andy <andrew.eliscu@gmail.com>
2023-01-08 18:16:57 -0600

    Home page

--- projects/gym_exercises/src/pages/Home.js
+++ projects/gym_exercises/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
+
 const Home = () => {
   return (
     <Box>

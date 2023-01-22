3c803cbd0130436f1b4b26aeb48641d75d9d26fa
Andy <andrew.eliscu@gmail.com>
2023-01-22 09:25:56 -0600

    New project setup

---
+++ projects/personal-page-v4.1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Andy LSQ</title>
+	<link rel="stylesheet" href="styles/main.css">
+</head>
+
+<body>
+
+	<?php include ( "templates/modules/header.php");?>
+
+	<main>
+		
+	</main>
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>

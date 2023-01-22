8ca6a99fc9882b175f825e162b88b45b912a635b
Andy <andrew.eliscu@gmail.com>
2023-01-22 11:02:58 -0600

    header content

--- projects/personal-page-v4.1/templates/modules/header.php
+++ projects/personal-page-v4.1/templates/modules/header.php
@@ -1,19 +1,19 @@
 <header class="l-header" id="header">
-    <nav class="nav bd-container">
-        
-        <a href="#" class="nav__logo">LSQ</a>
+	<nav class="nav bd-container">
+		
+		<a href="#" class="nav__logo">LSQ</a>
 
-        <div class="nav__menu" id="nav-menu">
-            <ul class="nav__list">
-                <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
-                <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
-                <li class="nav__item"><a href="#portfolio" class="nav__link">Portfolio</a></li>
-                <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
-            </ul>
-        </div>
+		<div class="nav__menu" id="nav-menu">
+			<ul class="nav__list">
+				<li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
+				<li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+				<li class="nav__item"><a href="#portfolio" class="nav__link">Portfolio</a></li>
+				<li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+			</ul>
+		</div>
 
-        <div class="nav__toggle" >
-            <i class='bx bx-menu ' id="nav-toggle"></i>
-        </div>
-    </nav>
+		<div class="nav__toggle" >
+			<i class="uil uil-bars" id="nav-toggle"></i>
+		</div>
+	</nav>
 </header>

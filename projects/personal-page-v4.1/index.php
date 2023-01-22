5211d870afb7dd16ac9ede978ce35efcaaa40a37
Andy <andrew.eliscu@gmail.com>
2023-01-22 11:03:32 -0600

    Link fonts, icons, and stylesheets

--- projects/personal-page-v4.1/index.php
+++ projects/personal-page-v4.1/index.php
@@ -4,7 +4,17 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Andy LSQ</title>
-	<link rel="stylesheet" href="styles/main.css">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/main.css">
+
+	<!-- Icons -->
+	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -19,5 +29,7 @@
 		
 	</footer>
 
+	<!--===== Main JS =====-->
+   <script src="js/main.js"></script>
 </body>
 </html>

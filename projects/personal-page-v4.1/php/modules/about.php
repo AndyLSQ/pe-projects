068504489fed78c6513184305f0daea601d15174
Andy <andrew.eliscu@gmail.com>
2023-01-22 22:22:23 -0600

    About module content

---
+++ projects/personal-page-v4.1/php/modules/about.php
@@ -0,0 +1,24 @@
+<section class="about section bd-container" id="about">
+	<div class="section-header">
+	 <span class="section-subtitle">My history</span>
+	 <h2 class="section-title">About me</h2>
+ </div>
+
+ <div class="about__container  bd-grid">
+     <div class="about__data bd-grid">
+         <h2>Who am I?</h2>
+         <p>I’m a front-end web developer based in Austin, Texas, and currently under the mentorship of <a href="https://perpetual.education/">Perpetual Education</a>, a program covering the entire design and web development process from idea to completion.</p>
+
+         <h2>Where have I been?</h2>
+         <p>I have 10 years experience as a management consultant and financial analyst, serving corporate clients of all sizes in <a href="https://www.accenture.com/us-en/insights/digital-transformation-index">digital transformation</a> and finance process architecture.</p>
+
+         <h2>Why am I here?</h2>
+         <p>I’m here to pursue a career path that aligns with my lifelong interest in design and technology. Take an in-depth look at my goals.</p>
+
+     </div>
+
+     <picture>
+	     <img src="images/square.jpg" alt="" class="about__img">
+     </picture>
+ </div>
+</section>

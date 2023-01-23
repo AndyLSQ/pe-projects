60bd5dfc9583dba27c4f6c2471c94b4a4eed27c1
Andy <andrew.eliscu@gmail.com>
2023-01-22 22:19:53 -0600

    Index updates

--- projects/personal-page-v4.1/index.php
+++ projects/personal-page-v4.1/index.php
@@ -14,15 +14,16 @@
 	<!-- Fonts -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=DM+Serif+Text:ital@0;1&family=Permanent+Marker&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-	<?php include ( "templates/modules/header.php");?>
+	<?php include ( "php/modules/header.php");?>
 
-	<main>
-		
+	<main class="l-main">
+		<!-- TODO: ADD ROUTER HERE FOR OTHER PAGES -->
+		<?php include ( "php/pages/home.php");?>
 	</main>
 
 	<footer>

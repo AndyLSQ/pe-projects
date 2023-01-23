20db4fa9615cef8e23ce347dca2fe53708398b90
Andy <andrew.eliscu@gmail.com>
2023-01-22 22:22:38 -0600

    Landing module content

---
+++ projects/personal-page-v4.1/php/modules/landing.php
@@ -0,0 +1,49 @@
+<section class="home" id="home">
+  <div class="home__container bd-container bd-grid">
+    <div class="home__data">
+      <div class="home__headline">
+        <span class="home__overline"></span>
+        <h1 class="home__title">Andy Eliscu</h1>
+        <span class="home__subtitle">(Pronounced L-S-Q)</span>
+      </div>
+      <p class="home__description">Front End Web Developer pursuing lifelong passions in technology &&nbsp;design. </p>
+      <a download="" href="#portfolio" class="button button-light home__button">See my work</a>
+
+    <div class="home__social">
+        <a href="https://www.linkedin.com/in/andreweliscu/" class="home__social-icon" target="linkedin">
+          <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/AndyLSQ" class="home__social-icon" target="github">
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="#contact" class="home__social-icon">
+            <i class="uil uil-message"></i>
+        </a>
+    </div>
+
+    </div>
+
+    <div class="home__img">
+      <picture>
+        <img src="images/headshot1.png" alt="">
+      </picture>
+
+      <div class="home__social-alt">
+        <a href="https://www.linkedin.com/in/andreweliscu/" class="home__social-icon" target="linkedin">
+          <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/AndyLSQ" class="home__social-icon" target="github">
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href="#contact" class="home__social-icon">
+            <i class="uil uil-message"></i>
+        </a>
+      </div>
+
+    </div>
+  </div>
+</section>
